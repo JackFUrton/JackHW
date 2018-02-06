83283e4a32a8e8db5b32472552a66a83a1cf5252
Daniel Kempner <dkempner@mren.com>
2018-02-05 19:24:38 -0800

    inches to cm

--- UnitConversions.java
+++ UnitConversions.java
@@ -3,18 +3,38 @@ import javax.swing.JOptionPane;
 public class UnitConversions {
 	public static void main(String[] args) {
 		String conversionType;
-
+		String finalUnit;
+	
 		conversionType = getConversionType();
+		
+		switch(conversionType) {
+		case "length":
+			finalUnit = getCentimetersOrInches();
+			break;
+		case "volume":
+			// code to ask....
+			break;
+		case "temperature":
+			// code to ask c or f
+			break;
+		}
 	}
 
 	public static String getConversionType() {
 		String[] options = { "length", "volume", "temperature" };
-		int n = JOptionPane.showOptionDialog(null, "Which unit of measure? ", "Integer Math",
+		int n = JOptionPane.showOptionDialog(null, "Which unit of measure? ", "Conversion Type",
 				JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
 
 		
 		return options[n];
+	}
+	
+	public static String getCentimetersOrInches() {
+		String[] options = {"Centimeters to Inches", "Inches to Centimeters" };
+		int n = JOptionPane.showOptionDialog(null, "", "",
+				JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
 
-
+		
+		return options[n];
 	}
 }
